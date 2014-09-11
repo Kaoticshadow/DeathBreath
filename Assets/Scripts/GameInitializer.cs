547a061c1675a80a01a51c6534b30447ad2dca31
Cherubyx <bloodbutterfly@gmail.com>
2014-09-11 12:05:37 -0400

    Adding basic Unity project setup.

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GameInitializer : MonoBehaviour {
+
+    MenuManager menuManager;
+ 
+    void Awake()
+    {
+        if (!MenuManager.Instance)
+        {
+            MenuManager menuMger = Instantiate(menuManager) as MenuManager;
+            DontDestroyOnLoad(menuMger);
+        }
+    }
+
+    void Update()
+    {
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+    private static MenuManager sInstance;
+    public static MenuManager Instance
+    {
+        get { return sInstance; }
+    }
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
