28e64cc71ce31be300f8ec65e6aa60867024bfdf
Tiffany Ip <tiffany.ip18@gmail.com>
2014-09-22 12:16:29 -0400

    Adding Tower, LevelManager prefabs.

---
+++ Assets/Scripts/Game/Tower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Tower : MonoBehaviour
+{
+    private Transform _towerTransform;
+
+    void Awake()
+    {
+        _towerTransform = transform;
+    }
+
+        
+}
---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour {
+
+    public KnightScript KnightUnit;
+    public Tower TowerUnit;
+
+    private List<KnightScript> _knightUnits = new List<KnightScript>();
+    private List<Tower> _towerUnits = new List<Tower>();
+
+    private static LevelManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_knightUnits.Count < 2)
+        {
+            KnightScript k = Instantiate (KnightUnit, new Vector3(Random.Range(-13f, 13f), Random.Range(-0.01f, -5.0f)), Quaternion.identity) as KnightScript;
+            _knightUnits.Add(k);
+        }
+
+        if (_towerUnits.Count < 2)
+        {
+
+        }
+	}
+}
