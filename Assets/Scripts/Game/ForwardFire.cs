3030015d58be3a4162e9dede9b47199bf4fb7128
Timu <tesmith@gmail.com>
2014-09-22 10:28:33 -0400

    Added linefire with 'ammunition' system, moved fireball to rmb.

--- Assets/Scripts/Game/ForwardFire.cs
+++ Assets/Scripts/Game/ForwardFire.cs
@@ -18,36 +18,17 @@ public class ForwardFire : MonoBehaviour {
 	void Start () {
 		m_t = this.transform;
 		speed = 0.3f;
-		fireSpeed = 10.0f;
+		fireSpeed = 25.0f;
 		//fireSpeed = 1.0f;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if (Input.GetKeyDown (KeyCode.Space)) {
-		//	Fire ();
-		//}
+
+	
 		
-		if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
-		{
-			m_t.localRotation = Quaternion.identity;
-			//_direction = new Vector3(1f, 0f, 0f);
-		}
-		
-		if (Input.GetKeyDown(KeyCode.S))
-		{
-			m_t.Rotate(0f, 0f, -35f);
-			//_direction = new Vector3(1f, -1f, 0f);
-		}
-		
-		if (Input.GetKeyDown(KeyCode.W))
-		{
-			m_t.Rotate(0f, 0f, 35f);
-			//_direction = new Vector3(1f, 1f, 0f);
-		}
-		
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(1))
 		{
 			_direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - m_t.position;
 			if (_direction != Vector3.zero)
---
+++ Assets/Scripts/Game/LineFire.cs
@@ -0,0 +1,93 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class LineFire : MonoBehaviour {
+	
+	
+	public bool stoppable;
+	public float length;
+
+	Transform m_t;
+	public float speed;
+	public float fireSpeed;
+	public FireBall m_rb;
+	public Cursor m_Cursor;
+	public LineRenderer fireLine;
+	public LineRenderer m_lined;
+	public GameObject Ammo;
+	Transform ammoTransform;
+
+	float ammoRate = 0.05f;
+
+	float ammoMax = 10f;
+	float ammoMin = 0f;
+
+	private Vector3 _direction = new Vector3 (1f, 0f, 0f);
+	
+	// Use this for initialization
+	void Start () {
+		m_t = this.transform;
+		m_t.position = m_t.position + new Vector3 (0, 0, 0.1f);
+		speed = 0.3f;
+		fireSpeed = 10.0f;
+		//fireSpeed = 1.0f;
+		Fire ();
+		ammoTransform = Ammo.transform;
+		ammoTransform.localScale = new Vector3(ammoMax,1,1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 baseData = new Vector3 (m_t.position.x, m_t.position.y, -1);
+		Vector3 lineData;
+		//Vector3 offset = new Vect
+		if (Input.GetMouseButton (0)) {
+			if(ammoTransform.localScale.x > ammoMin){
+				lineData = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, -1));
+				
+				lineData.z = -1;
+
+				if(stoppable){
+					if(Vector3.Distance(lineData, baseData)>length)
+					{
+						Debug.Log(Vector3.Distance(lineData,baseData));
+						Vector3 norm = lineData-baseData;
+
+						Debug.Log (norm);	
+						lineData = baseData + (norm.normalized*length);
+					}
+				
+				}
+
+				lineData.z = -1;
+				m_lined.SetPosition (0, baseData);
+				m_lined.SetPosition (1, lineData);
+			if(ammoTransform.localScale.x >ammoMin)
+				ammoTransform.localScale = new Vector3(ammoTransform.localScale.x - ammoRate, 1, 1);
+			}
+			else{
+			lineData = new Vector3(0,0,0);
+			m_lined.SetPosition (0,lineData);
+			m_lined.SetPosition (1,lineData);
+			}
+		} else {
+			lineData = new Vector3(0,0,0);
+			m_lined.SetPosition (0,lineData);
+			m_lined.SetPosition (1,lineData);
+
+			if(ammoTransform.localScale.x < ammoMax)
+				ammoTransform.localScale = new Vector3(ammoTransform.localScale.x + 2*ammoRate, 1, 1);
+		}
+	}
+	
+	void Fire () 
+	{
+		LineRenderer fireClone = Instantiate (fireLine, m_t.position, m_t.rotation) as LineRenderer;
+		m_lined = fireClone;
+	}
+}
+
