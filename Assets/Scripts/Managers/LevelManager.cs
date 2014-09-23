9a1438ea59b98c4d2364a89851e8058839371b0e
louismclean <louis.mclean@gmail.com>
2014-09-23 11:56:41 -0400

    Music is now a jarring, though catchy, one minute loop. Added a win condition. Randomized enemy spawning a bit. Consider this turd polished!

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -13,6 +13,8 @@ public class LevelManager : MonoBehaviour {
     private static LevelManager Instance;
     private enemyHealth healthScript;
     private float towerEndXLocation = -13.0f;
+	private int knightSpawnTimer = 0;
+	private int remainingKnights = 15;
 
     void Awake()
     {
@@ -23,12 +25,18 @@ public class LevelManager : MonoBehaviour {
 	void Update () {
         CheckEnemyHealth();
         CheckTowerPosition();
-        if (_knightUnits.Count < 2)
+		if (knightSpawnTimer > 0) 
+		{
+			knightSpawnTimer--;
+		}
+
+		if (_knightUnits.Count < 3 && knightSpawnTimer <= 0 && _knightUnits.Count < remainingKnights)
         {
-            _knightUnits.Add(Instantiate(KnightUnit, new Vector2(UnityEngine.Random.Range(13f,23f), UnityEngine.Random.Range(-1.00f, -5.0f)), Quaternion.identity) as KnightScript);
+            _knightUnits.Add(Instantiate(KnightUnit, new Vector2(UnityEngine.Random.Range(13f,23f), UnityEngine.Random.Range(-2.00f, -6.0f)), Quaternion.identity) as KnightScript);
+			knightSpawnTimer = UnityEngine.Random.Range (50,500);
         }
 
-        if (_towerUnits.Count < 2)
+        if (_towerUnits.Count < 1)
         {
             Debug.Log("Spawning tower units");
             _towerUnits.Add(Instantiate(TowerUnit, new Vector2(13.0f, UnityEngine.Random.Range(-1.00f, -5.0f)), Quaternion.identity) as Tower);
@@ -45,11 +53,16 @@ public class LevelManager : MonoBehaviour {
 	            if (healthScript.health <= 0)
 	            {
 	                _knightUnits.RemoveAt(i);
+					remainingKnights--;
+					UpdateKnightsRemaining();
 	            }
 			}
+			//wait... why is this here
 			else
 			{
 				_knightUnits.RemoveAt(i);
+				remainingKnights--;
+				UpdateKnightsRemaining();
 			}
         }
     }
@@ -65,4 +78,15 @@ public class LevelManager : MonoBehaviour {
             }
         }
     }
+
+	void UpdateKnightsRemaining()
+	{
+		GameObject.Find ("KnightsRemaining").guiText.text  = "Knights Remaining: "+remainingKnights;
+
+		if (remainingKnights <= 0) {
+			GameObject.Find ("LosingText").guiText.text = "A winner is you!!";
+			GameObject.Find ("LosingText").guiText.color = Color.white;
+		}
+
+	}
 }
