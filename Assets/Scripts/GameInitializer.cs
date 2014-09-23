fc188cae99b0707d2cc06b35cdc368341d1d99cb
Tiffany Ip <tiffany.ip18@gmail.com>
2014-09-23 13:57:53 -0400

    Cleaned up master from prototype entries. BASELINE UNITY project setup.

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -4,19 +4,22 @@ using System.Collections;
 
 public class GameInitializer : MonoBehaviour {
 
-    MenuManager menuManager;
+    //Assign your managers that you want to setup on game init
+    //This allows for non cluttering of the scene file (DeathBreath.unity)
+    //public MenuManager menuManager;
  
     void Awake()
     {
-        if (!MenuManager.Instance)
-        {
-            MenuManager menuMger = Instantiate(menuManager) as MenuManager;
-            DontDestroyOnLoad(menuMger);
-        }
+        //if (!MenuManager.Instance)
+        //{
+        //    MenuManager menuMger = Instantiate(menuManager) as MenuManager;
+        //    DontDestroyOnLoad(menuMger);
+        //}
     }
 
     void Update()
     {
+        //Remove this initializer when everything else is created in the scene
         Destroy(this);
     }
 }
