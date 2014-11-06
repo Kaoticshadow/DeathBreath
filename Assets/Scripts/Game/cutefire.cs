a3afd0cf19776cc78529b76731c7373bdbb67871
louismclean <louis.mclean@gmail.com>
2014-11-06 15:40:57 -0500

    cutefire

---
+++ Assets/Scripts/Game/cutefire.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cutefire : MonoBehaviour {
+
+	public float lifetime = 5f;
+	// Use this for initialization
+	void Start () {
+		this.rigidbody2D.AddTorque (Random.Range (2000f, 5000f));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime -= Time.deltaTime;
+		if (lifetime <= 0) {
+			Destroy(this.gameObject);
+		}
+	
+	}
+}
