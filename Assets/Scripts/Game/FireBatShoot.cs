9f5b2238c38a0c79ed30847c9a31366acf1b5a62
Marco Sabini <marcosabini@hotmail.com>
2014-11-06 16:09:36 -0500

    firebat shoot script complete

---
+++ Assets/Scripts/Game/FireBatShoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBatShoot : MonoBehaviour {
+
+	GameObject go;
+	public cutefire cf_pf;
+	float myRand;
+	public float fireDelay = 1.0f;
+	float fireCounter;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		go = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fireCounter += Time.deltaTime;
+		if(fireCounter >= fireDelay){
+			fire();		
+		}
+	}
+
+	void fire(){
+		
+		cutefire c = Instantiate (cf_pf, go.transform.position, Quaternion.identity) as cutefire;
+		c.rigidbody2D.AddForce (new Vector2 (-300f, 0));
+		fireCounter = 0;
+
+	}
+}
