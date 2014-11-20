b04d5014918ec8bd6f62ec4322377e4370e5095c
louismclean <louis.mclean@gmail.com>
2014-11-20 17:10:13 -0500

    WIP extra player projectiles + "Extra Tags" system! (you're welcome, Marco)

--- Assets/Scripts/Game/DragonShoot.cs
+++ Assets/Scripts/Game/DragonShoot.cs
@@ -8,6 +8,8 @@ public class DragonShoot : MonoBehaviour {
 
 	public Flame f;
 	public Flame of;
+	public Flame smallFire;
+	public Flame o_smallFire;
 	public float fireForce = 3600f;
 
 	public float cooldown = 0.2f;
@@ -74,25 +76,32 @@ public class DragonShoot : MonoBehaviour {
 		case 0:
 			break;
 		case 1:
-			Flame x = (Flame)Instantiate (of, startloc.transform.position, Quaternion.identity);
+			Flame x = (Flame)Instantiate (smallFire, startloc.transform.position, Quaternion.identity);
 			x.transform.localScale = new Vector2(x.transform.localScale.x,x.transform.localScale.y*fireScale);
 			x.rigidbody2D.AddForce (new Vector2 (fireForce, 0));
-			coolingdown = true;
-			x.power = fireScale;
+			//coolingdown = true;
+			x.power = fireScale / 5f;
 			break;
-		case 2: 
+		case 2:
+			Flame x3 = (Flame)Instantiate (of, startloc.transform.position, Quaternion.identity);
+			x3.transform.localScale = new Vector2(x3.transform.localScale.x,x3.transform.localScale.y*fireScale);
+			x3.rigidbody2D.AddForce (new Vector2 (fireForce, 0));
+			//coolingdown = true;
+			x3.power = fireScale;
+			break;
+		case 3: 
 			Flame x1 = (Flame)Instantiate (f, startloc.transform.position, Quaternion.identity);
 			x1.transform.localScale = new Vector2(x1.transform.localScale.x,x1.transform.localScale.y*fireScale);
 			x1.rigidbody2D.AddForce (new Vector2 (fireForce, fireForce*0.7f));
-			coolingdown = true;
+			//coolingdown = true;
 			x1.power = fireScale;
 			Flame x2 = (Flame)Instantiate (f, startloc.transform.position, Quaternion.identity);
 			x2.transform.localScale = new Vector2(x2.transform.localScale.x,x2.transform.localScale.y*fireScale);
 			x2.rigidbody2D.AddForce (new Vector2 (fireForce, -fireForce*0.7f));
-			coolingdown = true;
+			//coolingdown = true;
 			x2.power = fireScale;
 			break;
-		case 3: 
+		case 4: 
 			break;
 		default:
 			break;
