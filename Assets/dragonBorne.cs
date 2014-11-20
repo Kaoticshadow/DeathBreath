36484e9e7c1f38a7658c5e865d83feed01f88506
Tim Smith <tesmith@gmail.com>
2014-11-20 16:59:57 -0500

    completed phase 1, partway phase 2

--- Assets/dragonBorne.cs
+++ Assets/dragonBorne.cs
@@ -15,14 +15,24 @@ public class dragonBorne : MonoBehaviour {
 	float axcd;
 	public GameObject fu;
 	public GameObject fusorigin;
+	public float phase1Dur = 10f;
+	public float phase2Dur = 10f;
+	public GameObject exitSpot;
+	public GameObject entry;
+	float returnctr = 3f;
+	bool phase2ready = false;
+	float phasectr;
+	bool ready2enter = false;
 	// Use this for initialization
 	void Start () {
 		stage = 0;
 		phase0Target = new Vector3 (5, 0, 0);
 		axcd = axfrequency;
 		shtcd = shoutfrequ;
+		phasectr = phase1Dur;
 		player = GameObject.FindGameObjectWithTag ("Player");
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,6 +41,8 @@ public class dragonBorne : MonoBehaviour {
 				break;
 			case 1: phase1 ();
 				break;
+		case 2: phase2();
+			break;
 			default:
 				break;
 		}
@@ -48,9 +60,36 @@ public class dragonBorne : MonoBehaviour {
 		g.GetComponent<Rigidbody2D> ().AddForce (-dir.normalized * 800f);
 	}
 
+	void phase2(){
+						phase2start ();
+		if (phase2ready) {
+
+				}
+
+	}
+
+
+	void phase2start(){
+		returnctr -= Time.deltaTime;
+		Debug.Log (returnctr);
+		if (this.transform.position != exitSpot.transform.position && returnctr > 0)
+				this.transform.position = Vector3.Lerp (this.transform.position, exitSpot.transform.position, 0.01f);
+		else {
+				this.transform.position = Vector3.Lerp (this.transform.position, entry.transform.position, 0.01f);
+				phase2ready = true;
+				this.transform.localScale = new Vector3(-1,1,1);
+		}
+
+		
+	}
+
 	void phase1(){
 		this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, Mathf.Min( 4.5f,player.transform.position.y + verticaloffset),.1f),0.05f);
-
+		phasectr -= Time.deltaTime;
+		if (phasectr < 0) {
+			phasectr = phase2Dur;
+			stage = 2;
+				}
 		shtcd -= Time.deltaTime;
 		axcd -= Time.deltaTime;
 		if (axcd < 0) {
