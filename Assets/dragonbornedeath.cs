e50b27bd99db8be50852cc301763af53809b387b
Tim Smith <tesmith@gmail.com>
2014-11-21 11:07:19 -0500

    Added death animation script to dragonborne

---
+++ Assets/dragonbornedeath.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragonbornedeath : MonoBehaviour {
+
+	Enemy e;
+	GameObject player;
+	public dragonsoul ds;
+	float cd = 1;
+	bool start = false;
+	// Use this for initialization
+	void Start () {
+		e = this.GetComponent<Enemy> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkDeath ();
+		if (start) {
+			cd-=Time.deltaTime;
+			if(cd<0)
+				Destroy(this.gameObject);
+			//spawn a ton of particles that arc towards player
+			dragonsoul temp = Instantiate(ds, this.transform.position,Quaternion.identity) as dragonsoul;
+			temp.target = player;
+			Vector3 dir = player.transform.position-this.transform.position ;
+			temp.rigidbody2D.AddForce((dir.normalized + new Vector3(Random.Range(-dir.normalized.x,dir.normalized.x)*.5f,
+			                                                       Random.Range(-dir.normalized.y,dir.normalized.y)*3,
+			                                                        0)*50)*10);
+
+
+		}
+	}
+
+	void checkDeath(){
+		if (e.health <= 0)
+						beginAnimation ();
+	}
+
+	void beginAnimation(){
+		start = true;
+
+	}
+}
---
+++ Assets/dragonsoul.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragonsoul : MonoBehaviour {
+
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateRotation ();
+		Vector3 dir = target.transform.position-this.transform.position;
+		if (target != null) {
+			this.rigidbody2D.AddForce(dir.normalized * 18f);//dir of player target
+				}
+		if (dir.magnitude < 1.5f)
+						Destroy (this.gameObject);
+	}
+
+
+	void updateRotation(){
+		Vector2 targetVector = this.rigidbody2D.velocity;
+		float angle = (Mathf.Atan2(targetVector.y, targetVector.x) * Mathf.Rad2Deg);
+		angle += 270.0f;
+		this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+}
