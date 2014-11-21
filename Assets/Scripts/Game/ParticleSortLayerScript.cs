b93acb76df706fcbca9b045e0380c20a672bc895
louismclean <louis.mclean@gmail.com>
2014-11-21 17:29:58 -0500

    Further embetterment of particle sort layer script

--- Assets/Scripts/Game/ParticleSortLayerScript.cs
+++ Assets/Scripts/Game/ParticleSortLayerScript.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class ParticleSortLayerScript : MonoBehaviour {
 
 	public string sortingLayerName = "Middle";
+	public int sortingLayerOrder = 0;
 	
 	// Use this for initialization
 	void Start () {
 		//Change Foreground to the layer you want it to display on
 		//You could prob. make a public variable for this
 		particleSystem.renderer.sortingLayerName = sortingLayerName;
+		particleSystem.renderer.sortingOrder = sortingLayerOrder;
 	}
 	
 	// Update is called once per frame
