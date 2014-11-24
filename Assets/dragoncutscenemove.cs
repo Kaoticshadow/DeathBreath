906663d94deccea16b86a6eb06d886dc160ecbac
Tim Smith <tesmith@gmail.com>
2014-11-24 12:35:58 -0500

    final cutscene + more audio

---
+++ Assets/dragoncutscenemove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragoncutscenemove : MonoBehaviour {
+	public Vector3 destination;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector3.Lerp (this.transform.position, destination, speed*Time.deltaTime);
+	}
+}
---
+++ Assets/twosceneaudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class twosceneaudio : MonoBehaviour {
+	bool changed = false;
+	public GameObject c;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.loadedLevelName == "throneroom"&&!changed){
+			c = GameObject.FindGameObjectWithTag("MainCamera");
+			changed = true;
+			this.transform.parent = c.gameObject.transform;
+		}
+	//	this.transform.position = c.transform.position;
+		if (Application.loadedLevelName == "final destination")
+						Destroy (this.gameObject);
+	}
+}
