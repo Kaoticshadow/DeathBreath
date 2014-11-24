770d035b9646f5bdf719e9268cd68e5499349d69
Tim Smith <tesmith@gmail.com>
2014-11-23 21:01:01 -0500

    More audio work, mostly on sky

--- Assets/audiofadein.cs
+++ Assets/audiofadein.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class audiofadein : MonoBehaviour {
 	float volume = 0;
 	AudioSource a;
+	public float max;
 	// Use this for initialization
 	void Start () {
 		a = this.GetComponent<AudioSource> ();
 		a.volume = volume;
+		if (max == 0)
+						max = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		a.volume = volume;
-		if (volume < 1)
+		if (volume < max)
 						volume += 0.01f;
 	}
 }
