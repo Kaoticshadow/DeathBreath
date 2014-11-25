a12607b6914a907a8bed8f7fdb0305814bc68764
Louis McLean <louis.mclean@gmail.com>
2014-11-24 22:52:12 -0500

    Huts spawn busted hut

--- Assets/Scripts/Game/Hut.cs
+++ Assets/Scripts/Game/Hut.cs
@@ -7,6 +7,7 @@ public class Hut : MonoBehaviour {
 	public GameObject spicy_chicken;
 	public GameObject dragon_fruit;
 	public GameObject hot_pepper;
+	public GameObject busted_hut;
 	GameObject spawnObject;
 	LevelManager levelManager;
 
@@ -31,7 +32,7 @@ public class Hut : MonoBehaviour {
 			spawnObject = dragon_fruit;
 			break;
 		default:
-			spawnObject = null;
+			spawnObject = busted_hut;
 			break;
 		}
 	}
@@ -43,10 +44,14 @@ public class Hut : MonoBehaviour {
 
 	void takeDamage(float damage){
 		health -= damage;
+		GameObject spawnedObject;
 		if (health < 0)
 		{
 			if(spawnObject != null){
-				Instantiate(spawnObject,this.transform.localPosition,Quaternion.identity);
+				spawnedObject = Instantiate(spawnObject,this.transform.localPosition,Quaternion.identity) as GameObject;
+				if(spawnObject == busted_hut){
+					spawnedObject.transform.localScale = this.transform.localScale;
+				}
 			}
 			//	GameObject.FindGameObjectWithTag("PopupScore").GetComponent<PopupScore>().popScore(this.transform.position);
 			Destroy (this.gameObject);
